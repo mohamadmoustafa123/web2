3e90d7362837eca2b460f390253c625b452fb031
Chaaban Daher <chaabendaher@gmail.com>
2025-11-22 23:21:22 +0200

    i added setName and Setpassord

--- src/components/signin.jsx
+++ src/components/signin.jsx
@@ -17,13 +17,14 @@ function Signin() {
         <h1>welcome</h1>
         <form onSubmit={handleSubmit}>
         <label htmlFor="name" >Name:</label>
-        <input type="text"  id='name' value={name} placeholder='enter your name' />
-
+        <input type="text"  id='name' value={name}  onChange={(e)=>SetName(e.target.value)} placeholder='enter your name' />
+          
          <label htmlFor="email">Email:</label>
-        <input type="email" id='email' value={password} placeholder='enter your name' />
+        <input type="email" id='email' value={password} placeholder='enter your Email' />
 
+        
          <label htmlFor="password">Password:</label>
-        <input type="email" id='password' placeholder='enter your name' />
+        <input type="password" id='password'  onChange={(e)=>SetPassord(e.target.value)}  placeholder='enter your password' />
 
         <button type='submit'>Submit</button>
         </form>
