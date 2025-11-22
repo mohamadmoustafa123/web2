d35fffde6c1d94d6e5b4908e82d3b6ea650fc02c
Mohamad moustafa <52231128@students.liu.edu.lb>
2025-11-22 11:46:11 +0200

    I installed the router ,created a homePage,and assigned it some style

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './components/HomePage';
 
 function App() {
   return (
     <div className="App">
 
+<Routes>
+  <Route path='/HomePage' Component={HomePage}/>
+</Routes>
     </div>
   );
 }
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -2,7 +2,11 @@ export default function HomePage(){
     return(
     <div className="App" style={
         {
-            
+            display:"flex",
+            justifyContent:"center",
+            alignItems:"center",
+            height:"100vh",
+            background:"#191b1f"
         }
     }>
 
