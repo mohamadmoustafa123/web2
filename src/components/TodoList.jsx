aba7fa9b730cbcdf5df08cf8b8849f2c75cfd283
Mohamad moustafa <52231128@students.liu.edu.lb>
2025-11-22 12:03:49 +0200

    i used material ui typography,and divider is a line from material ui

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,10 +1,15 @@
-import { Card, Container } from "@mui/material";
+import { Card, CardContent, Container, Divider, Typography } from "@mui/material";
 
 export default function TodoList(){
     return(
        <>
        <Container maxWidth="sm">
         <Card sx={{minWidth:275} }style={{maxHeight:"80vh",overflow:"scroll"}}>
+            <CardContent>
+                <Typography variant="h2" style={{ fontWeight: "normal" }}>My Tasks</Typography>
+                <Divider/>
+                
+            </CardContent>
 
         </Card>
 
