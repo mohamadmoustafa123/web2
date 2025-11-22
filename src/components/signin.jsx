b916621dfcd07fed99515817eca9931a69a7cb4a
Chaaban Daher <chaabendaher@gmail.com>
2025-11-22 22:47:27 +0200

    I added a button to submit

--- src/components/signin.jsx
+++ src/components/signin.jsx
@@ -3,10 +3,21 @@ import React from 'react'
 function signin() {
   return (
     <div>
-        <div>
+       <div>        
         <h1>welcome</h1>
-        <label htmlFor="name">Name</label>
-        <input type="text" placeholder='enter your name' />
+        <form>
+        <label htmlFor="name" >Name</label>
+        <input type="text"  id='name' value={name} placeholder='enter your name' />
+
+         <label htmlFor="email">Email</label>
+        <input type="email" id='email' placeholder='enter your name' />
+
+         <label htmlFor="password">Password</label>
+        <input type="email" id='password' placeholder='enter your name' />
+
+        
+
+        </form>
         </div>
     </div>
   )
