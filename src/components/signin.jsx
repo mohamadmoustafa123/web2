3e4e92f12c30399ed7999a3987505eec8f77727d
Chaaban Daher <chaabendaher@gmail.com>
2025-11-22 23:10:04 +0200

    I added to form onSubmit to information addetion

--- src/components/signin.jsx
+++ src/components/signin.jsx
@@ -1,26 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function signin() {
+function Signin() {
+    const [name,SetName]=useState("");
+    const [password,SetPassord]=useState("");
+    
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+       console.log("Name:",name);
+       console.log("Password:",password);
+    }
+
+
   return (
     <div>
        <div>        
         <h1>welcome</h1>
-        <form>
-        <label htmlFor="name" >Name</label>
+        <form onSubmit={handleSubmit}>
+        <label htmlFor="name" >Name:</label>
         <input type="text"  id='name' value={name} placeholder='enter your name' />
 
-         <label htmlFor="email">Email</label>
-        <input type="email" id='email' placeholder='enter your name' />
+         <label htmlFor="email">Email:</label>
+        <input type="email" id='email' value={password} placeholder='enter your name' />
 
-         <label htmlFor="password">Password</label>
+         <label htmlFor="password">Password:</label>
         <input type="email" id='password' placeholder='enter your name' />
 
-        
-
+        <button type='submit'>Submit</button>
         </form>
         </div>
     </div>
   )
 }
 
-export default signin
+export default Signin
