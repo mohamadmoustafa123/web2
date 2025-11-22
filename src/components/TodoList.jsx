47532d438805fd4e63ba6c111d4c52f7229dd20e
Mohamad moustafa <52231128@students.liu.edu.lb>
2025-11-22 12:16:33 +0200

    I used the toggle button from Material UI, which has a value that I will use later to determine how the array will be based on it. Also, the onchange function is used to change the value whenever a button is pressed.

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,19 +1,45 @@
-import { Card, CardContent, Container, Divider, Typography } from "@mui/material";
+import {
+  Card,
+  CardContent,
+  Container,
+  Divider,
+  ToggleButton,
+  ToggleButtonGroup,
+  Typography,
+} from "@mui/material";
+import { useState } from "react";
 
-export default function TodoList(){
-    return(
-       <>
-       <Container maxWidth="sm">
-        <Card sx={{minWidth:275} }style={{maxHeight:"80vh",overflow:"scroll"}}>
-            <CardContent>
-                <Typography variant="h2" style={{ fontWeight: "normal" }}>My Tasks</Typography>
-                <Divider/>
-                
-            </CardContent>
-
+export default function TodoList() {
+    const [displayedTodosType,setDispledTodosType]=useState("all")
+    function handleChange(e){
+        setDispledTodosType(e.target.value)
+    }
+  return (
+    <>
+      <Container maxWidth="sm">
+        <Card
+          sx={{ minWidth: 275 }}
+          style={{ maxHeight: "80vh", overflow: "scroll" }}
+        >
+          <CardContent>
+            <Typography variant="h2" style={{ fontWeight: "normal" }}>
+              My Tasks
+            </Typography>
+            <Divider />
+            <ToggleButtonGroup
+              color="primary"
+              value={displayedTodosType}
+              exclusive
+              onChange={handleChange}
+              aria-label="Platform"
+            >
+              <ToggleButton value="all">All</ToggleButton>
+              <ToggleButton value="completed">Completed</ToggleButton>
+              <ToggleButton value="non-completed">Non-Completed</ToggleButton>
+            </ToggleButtonGroup>
+          </CardContent>
         </Card>
-
-       </Container>
-       </>
-    )
+      </Container>
+    </>
+  );
 }
