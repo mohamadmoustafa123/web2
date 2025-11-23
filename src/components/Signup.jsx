97ebe0f25285873034448ef6b2d2e7d94bdd47cf
Chaaban Daher <chaabendaher@gmail.com>
2025-11-23 11:12:40 +0200

    i addded h1 to identification

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,14 +1,33 @@
+import { Email } from '@mui/icons-material';
 import React from 'react'
-
- const [name,SetName]=useState("");
- const [email,SetEmail]=useState("");
- const [password,SetPassord]=useState("");
- const [confirmPassword,SetConfirmPassord]=useState("");
-
-
+ 
 function Signup() {
+   const [name,SetName]=useState("");
+   const [email,SetEmail]=useState("");
+   const [password,SetPassord]=useState("");
+   const [confirmPassword,SetConfirmPassord]=useState("");
+
+
+
+     const handleSubmit = (e)=>{
+       e.preventDefault();
+
+     if (password !==confirmPassword){
+       alert("password do not match");
+       return;
+     } 
+
+console.log("Name",name);
+console.log("Emamil",Email);
+console.log("password",password);
+
+}
   return (
-    <div>Signup</div>
+    <div>
+        <div>
+            <h1></h1>
+        </div>
+    </div>
   )
 }
 
