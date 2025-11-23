d99eece58a96ed37bb2549233e44c26006ade89a
Chaaban Daher <chaabendaher@gmail.com>
2025-11-23 11:29:55 +0200

    i do label and input to  email and password

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -28,9 +28,22 @@ console.log("password",password);
           <h1>welcome..</h1>
            <form onSubmit={handleSubmit}>
             <div>
-            <label htmlFor="name" className='block text-gray-700 mb-1'>Name:</label>
-            <input type="text"  id='name' value={name} onChange={(e)=>SetName(e.target.value)} placeholder='enter your name'className='w-full px-4 py-2 border-gray-300 rounded-lg focus:ring-blue-400' />
+             <label htmlFor="name" className='block text-gray-700 mb-1'>Name:</label>
+             <input type="text"  id='name' value={name} onChange={(e)=>SetName(e.target.value)} placeholder='enter your name'className='w-full px-4 py-2 border-gray-300 rounded-lg focus:ring-blue-400' />
             </div>
+            <div>
+             <label htmlFor="email" className='block text-gray-700 mb-1'>Email:</label>
+             <input type="text"  id='email' value={name} onChange={(e)=>SetEmail(e.target.value)} placeholder='enter your name'className='w-full px-4 py-2 border-gray-300 rounded-lg focus:ring-blue-400' />
+            </div>
+            <div>
+             <label htmlFor="password" className='block text-gray-700 mb-1'>password:</label>
+             <input type="password"  id='password' value={name} onChange={(e)=>SetPassord(e.target.value)} placeholder='enter your name'className='w-full px-4 py-2 border-gray-300 rounded-lg focus:ring-blue-400' />
+            </div>
+            <div>
+             <label htmlFor="confirmPassword" className='block text-gray-700 mb-1'>Confirm password:</label>
+             <input type="text"  id='confirmPassword' value={confirmPassword} onChange={(e)=>SetConfirmPassord(e.target.value)} placeholder='enter your name'className='w-full px-4 py-2 border-gray-300 rounded-lg focus:ring-blue-400' />
+            </div>
+            
          </form>
         </div>
     </div>
