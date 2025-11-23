e465e441938205401c3284c6f702656209639bdf
Mohamad moustafa <52231128@students.liu.edu.lb>
2025-11-23 09:56:45 +0200

    I decided to add an NvBar to the project to increase the number of pages, so I put the pages inside a Router with a Navbar and put the outlet in it (the pages that will be displayed).

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-import { Route, Routes } from 'react-router-dom';
-import HomePage from './components/HomePage';
+import logo from "./logo.svg";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./components/HomePage";
+import NavBar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
 
-<Routes>
-  <Route path='/HomePage' Component={HomePage}/>
-</Routes>
+    <div className="App">
+      <Routes>
+        <Route Component={NavBar}>
+          <Route path="/HomePage" Component={HomePage} />
+        </Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+
+export default function NavBar(){
+    return(
+<>
+
+<div></div>
+<Outlet/>
+
+</>
+
+    )
+}
