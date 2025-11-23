9dd5dc00b1628646ba736f62bbdc51defe9c7211
Chaaban Daher <chaabendaher@gmail.com>
2025-11-23 10:54:05 +0200

    i added const to name email password and confirm password to useState

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
 
+ const [name,SetName]=useState("");
+ const [email,SetEmail]=useState("");
+ const [password,SetPassord]=useState("");
+ const [confirmPassword,SetConfirmPassord]=useState("");
+
+
 function Signup() {
   return (
     <div>Signup</div>
