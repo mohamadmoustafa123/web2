4881ef5ab48796d8eed59ceb29c08d4cc6e83c79
Mohamad moustafa <52231128@students.liu.edu.lb>
2025-12-30 19:39:27 +0200

    TEST

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,7 @@
 export default function About() {
   return (
     <>
+    
   <div style={{height:"100vh" ,display:"flex",flexDirection:"column",justifyContent:"center",alignItems:"center"}}>
         <h2 >
         This website is for people with busy daily schedules, helping them
